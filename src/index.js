f54d6b9c0a620ead7edddb54b4dc43aca6139280
Nikhil Singhal <niksinghal@paypal.com>
2022-03-15 09:20:20 -0700

    feat: exposing files from the module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 export moveCursorForward from './move_cursor_forward';
 export moveCursorUp from './move_cursor_up';
-export foreGroundColor from './foreground_color';
+export foregroundColor from './foreground_color';
+export moveCursorBack from './move_cursor_back';
+export moveCursorDown from './move_cursor_down';
+export MoveCursorHorizontally from './move_cursor_horizontally';
+export MoveCursorVertically from './move_cursor_vertically';
+export MoveCursorAt from './move_cursor_at';
+export { saveCursorPosition, restoreCursorPosition } from './save_restore_cursor_position';
